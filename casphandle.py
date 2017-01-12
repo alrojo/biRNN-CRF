a8210567971b0b80fd38e481555e4ba23070d3e3
Ubuntu <ubuntu@ip-172-31-35-200.eu-west-1.compute.internal>
2017-01-12 16:13:25 +0000

    casp handle update

--- casphandle.py
+++ casphandle.py
@@ -2,7 +2,9 @@ import csv
 import numpy as np
 import glob
 
+# all file names, we have about 228
 file_names = glob.glob("data/*/*.feat42")
+num_files = len(file_names)
 
 
 # we need to reorder the amino acids so it fits with the CB6133 style
@@ -13,32 +15,30 @@ pssm_dict = {'A':0, 'C':1, 'D':2, 'E':3, 'F':4, 'G':5, 'H':6, 'I':7, 'K': 8, 'L'
 pssm_order = [pssm_dict[i] for i in list("ARNDCQEGHILKMFPSTWYVX")]
 
 ss_dict = {'L':0, 'B':1, 'E':2, 'G':3, 'I':4, 'H':5, 'S':6, 'T':7}
-def onehot_ss(name):
-    idx = ss_dict[name]
-    return out
 
+# reorder acording to a list of intergers (e.g. pssm_order)
 def reorder_list(mylist, myorder):
     return [mylist[i] for i in myorder]
 
-
-num_files = len(file_names)
+# setup data holders
 X = np.zeros((num_files, 700, 42))
 t = np.zeros((num_files, 700))
 mask = np.zeros((num_files, 700))
 
+# go through each file and fill information into data holders
 for idx, file_name in enumerate(file_names):
-    reader = csv.reader(open(file_name), delimiter="\t")
+    reader = csv.reader(open(file_name), delimiter="\t") # load data
     my_dict = {}
     for row in reader:
         fix = row[0].strip().split(' ')
-        jan = [float(i) for i in reorder_list(fix[3:24], aa_order)]
-        jan = np.array(jan) # AA
-        jun = [float(i) for i in reorder_list(fix[24:], pssm_order)]
-        jun = np.asarray(jun) # PSSM
+        aa_elem = [float(i) for i in reorder_list(fix[3:24], aa_order)]
+        aa_elem = np.array(aa_elem) # AA
+        pssm_elem = [float(i) for i in reorder_list(fix[24:], pssm_order)]
+        pssm_elem = np.asarray(pssm_elem) # PSSM
         my_dict[int(fix[0])-1] = (fix[1], # amino acid residue
                                   ss_dict[fix[2]],
-                                  jan,
-                                  jun)
+                                  aa_elem,
+                                  pssm_elem)
     X_part = np.zeros((700, 42))
     t_part = np.zeros((700,))
     mask_part = np.zeros((700,))
@@ -52,6 +52,7 @@ for idx, file_name in enumerate(file_names):
     t[idx] = t_part
     mask[idx] = mask_part
 
+# make data accessible with a function
 def get_data():
     return X, t, mask
 
